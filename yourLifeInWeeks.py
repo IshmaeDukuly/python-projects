42df8cf08d2160514a73c0214f8b1fcd05f340cf
IshmaeDukuly <oisemadukuly82@gmail.com>
2023-06-04 10:12:02 +0000

    yourlifeInWeeks

---
+++ yourLifeInWeeks.py
@@ -0,0 +1,13 @@
+
+
+# The programme calculates the number of days, weeks, and months you will if your were to live 100 years
+
+age_as_int = int(input("What is your current Age?: "))
+years_remaining = 100 - age_as_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+
+message = f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months"
+print(message)
