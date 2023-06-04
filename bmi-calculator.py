a87f678f9a4d8d7126b4343d89f37476aca86cf3
IshmaeDukuly <oisemadukuly82@gmail.com>
2023-06-04 10:14:17 +0000

    bmi-calculator

---
+++ bmi-calculator.py
@@ -0,0 +1,10 @@
+
+
+# A simple programme to calculate Body Mass Weight
+
+height = float(input("Enter your height in M: "))
+weight = int(input("Enter Your Weight IN Kg: "))
+
+
+bmi = weight/(height) ** 2
+print(bmi)
