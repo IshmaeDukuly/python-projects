d4879ef53347464d46a6b0d6fdaf1a2a185df77b
Ishmael M. Dukuly <99619343+IshmaeDukuly@users.noreply.github.com>
2023-06-06 12:22:15 +0000

    yearscalculator

---
+++ yearsCalculator.py
@@ -0,0 +1,10 @@
+
+# This is a simple age calculator
+
+person = input("what is your name? ")
+birthYear = int(input("Enter your birth year "))
+currentYear = int(input("Enter the current year "))
+    
+result = currentYear - birthYear
+    
+print(person, ", you are ", result, "years old ")
